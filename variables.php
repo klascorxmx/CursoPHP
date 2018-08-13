52785f0c10413a41cc8a2096e787a5aa7f2ceb70
Klascor <higuera2914@gmail.com>
2018-08-13 16:06:12 -0400

    archivo de comandos y practica de variables

--- variables.php
+++ variables.php
@@ -1,17 +1,37 @@
-<?php 
+<?php
+
+
+/*                  FORZAR VARIABLES int-string-float-array-object
+
+$a = 125.5;
+
+$b = (string)$a;
+
+echo var_dump($b);
+*/
+
 //string : cadena de texto se recomienda usar comillas simples pra declarar variables de tipo string
-$nombre = 'Franklin';
+// $nombre = 'Franklin';
 
 // integer: nomeros enteros
-$mensaje = 'te va a mandar a la intrnet';
+// $mensaje = 'te va a mandar a la intrnet';
 
 // float cuando son de 32 bit y double cuando son de 64 bit: numeros con decimales
 
-$edad = 34.5;
+//$edad = 34.5;
 // gettype muestra que tipo de variable es
 //var_dump esta funcion muestra que tipo de variale es y adicional muestra su valor  y su contenido
 
 // echo var_dump($nombre)
-echo "<a href='http://google.com'>".$nombre. "</a>".$mensaje;
+//echo "<a href='http://google.com'>".$nombre. "</a>".$mensaje;
+ /*
+CONDICIONALES IF  Y ELSE
+ $num = 10;
 
+ if ($num <= 9) {
+ 	echo "ES MENOR";
+ 	# code...
+ } else {
+ 	echo "ES MAYO QUE 9";
+ }*/
  ?>
