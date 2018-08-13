4a49fc5ec6f7ba55e22603c2c0d92923778329f1
klascor <higuera2914@gmail.com>
2018-08-12 21:22:57 -0400

    primer commit del curso de php

---
+++ hola mundo.php
@@ -0,0 +1,3 @@
+<?php 
+echo "hola mundo estoy enpezando un nuevo curso";
+ ?>
---
+++ variables.php
@@ -0,0 +1,17 @@
+<?php 
+//string : cadena de texto se recomienda usar comillas simples pra declarar variables de tipo string
+$nombre = 'Franklin';
+
+// integer: nomeros enteros
+$mensaje = 'te va a mandar a la intrnet';
+
+// float cuando son de 32 bit y double cuando son de 64 bit: numeros con decimales
+
+$edad = 34.5;
+// gettype muestra que tipo de variable es
+//var_dump esta funcion muestra que tipo de variale es y adicional muestra su valor  y su contenido
+
+// echo var_dump($nombre)
+echo "<a href='http://google.com'>".$nombre. "</a>".$mensaje;
+
+ ?>
